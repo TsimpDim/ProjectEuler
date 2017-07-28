cd36f6fb698a0dc9c481780cdc15bcfe628d594f
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-07-28 11:48:51 +0300

    Solved problem 6

---
+++ Problem6.py
@@ -0,0 +1,14 @@
+#sum of squares from 1...100
+sum_sq = (100*(100+1)*(2*100+1))/6
+print(sum_sq)
+
+#normal sum from 1...100
+sum_nrm = (100*(100+1))/2
+print(sum_nrm)
+
+#normal sum squared
+sum_nrm_sq = sum_nrm**2
+print(sum_nrm_sq)
+
+#difference
+print(sum_nrm_sq - sum_sq)
