95a14110b8fbc4d363b358c638ac606a07319d23
TsimpDim <dimitristsimpitas@yahoo.gr>
2017-08-05 21:02:05 +0300

    Solved problem 9

---
+++ Problem9.py
@@ -0,0 +1,29 @@
+def factors(value):
+    factors = []
+    for i in range(1, int(value**0.5)+1):
+        if value % i == 0:
+            factors.append((i, int(value / i)))
+    return factors
+
+def triple_sum(x,y,z):
+    return x+y+z
+
+
+i = 1
+triples = []
+summ = 0
+x,y,z=0,0,0
+while summ != 1000:
+    fac = factors(i*i/2)
+    for pair in fac:
+        x = i + pair[0]
+        y = i + pair[1]
+        z = i + pair[0] + pair[1]
+
+        triples.append([x,y,z])
+        if triple_sum(*[x,y,z]) == 1000: break
+    
+    summ = triple_sum(*[x,y,z])
+    i += 1
+print(x*y*z)       
+print(triples)
