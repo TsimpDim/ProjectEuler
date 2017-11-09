2c9561278cd1ea549249ef59f49db8adb60d6441
Tsimpitas Dimitris <dimitristsimpitas@yahoo.gr>
2017-11-09 10:45:40 +0200

    Fixed pep8 standards

--- Problem25.py
+++ Problem25.py
@@ -1,24 +1,21 @@
 
-def FirstNDigits(digits,print_limit):
+def FirstNDigits(digits, print_limit):
 
     p = 0
     c = 1
     n = p + c
     index = 2
 
-    print(c,n,sep='\n')
+    print(c, n, sep='\n')
     while True:
         p = c
         c = n
         n = p + c
         index += 1
-        print('F'+str(index), str(n)[:print_limit])
+        print('F' + str(index), str(n)[:print_limit])
 
         if len(str(n)) == digits:
             break
 
 
-
-FirstNDigits(1000,0)
-
-    
+FirstNDigits(1000, 0)
