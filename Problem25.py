2fa569498e3c6537291fbff310bf25f717ea6dfc
Tsimpitas Dimitris <dimitristsimpitas@yahoo.gr>
2017-11-09 10:40:41 +0200

    Solved problem 25

---
+++ Problem25.py
@@ -0,0 +1,24 @@
+
+def FirstNDigits(digits,print_limit):
+
+    p = 0
+    c = 1
+    n = p + c
+    index = 2
+
+    print(c,n,sep='\n')
+    while True:
+        p = c
+        c = n
+        n = p + c
+        index += 1
+        print('F'+str(index), str(n)[:print_limit])
+
+        if len(str(n)) == digits:
+            break
+
+
+
+FirstNDigits(1000,0)
+
+    
